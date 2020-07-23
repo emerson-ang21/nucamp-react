ee127c4a66e4f938286f2703927fd31b0d1d69c9
emerson-ang21 <emersonang91@gmail.com>
2020-07-23 01:55:57 -0700

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -18,7 +18,7 @@ function RenderCampsite({campsite}) {
             </div>
         );
     }
-function RenderComments({comments}){
+function RenderComments({comments, addComment, campsiteId}){
         if(comments){
             return (
                 <div className="col-md-5 m-1">
@@ -39,7 +39,7 @@ function RenderComments({comments}){
                     </div>
             );
         })}
-            <CommentForm />
+            <CommentForm campsiteId={campsiteId} addComment={addComment} />
         </div>
             );
         }
@@ -62,8 +62,7 @@ function RenderComments({comments}){
     
         handleSubmit(values) {
             this.toggleModal();
-            console.log("Current State is: " + JSON.stringify(values));
-            alert("Current State is: " + JSON.stringify(values));
+            this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
         }
     
         render() {
@@ -154,7 +153,11 @@ function RenderComments({comments}){
                     </div>
                     <div className="row">
                         <RenderCampsite campsite={props.campsite} />
-                        <RenderComments comments={props.comments} />
+                        <RenderComments 
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
                     </div>
                 </div>
             );
